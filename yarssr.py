c69cebdd8ec2c89671e3d2bb0f1e5739e722d564
Victor Peinado <vitojph@gmail.com>
2014-08-14 18:54:29 +0200

    changed the Spanish processing to use textalytics API

--- yarssr.py
+++ yarssr.py
@@ -4,7 +4,16 @@
 from flask import Flask
 from flask import render_template
 from pattern.web import Newsfeed, plaintext
+
+# for processing English
 from alchemyapi import AlchemyAPI
+
+# for processing Spanish
+import urllib
+import urllib2
+import json
+url = 'https://textalytics.com/core/topics'
+
 
 app = Flask(__name__)
 reader = Newsfeed()
@@ -38,6 +47,7 @@ def index():
     items.append(dict(site=feed[0], feedlist=feedlist))
   return render_template("index.html", items=items)
 
+
 @app.route('/es')
 def index_es():
   RSS_LIST = [
@@ -52,15 +62,29 @@ def index_es():
     feedlist = []
     for result in reader.search(feed[1])[:10]:
       clean_text = plaintext(result.text)
-      response = alchemyapi.entities("text", result.text)
+      params = urllib.urlencode({
+        'key': '4c4ded0a7c279c9f747a8f750e223363', # topic extraction
+        'of': 'json',
+        'lang': 'es',
+        'txt': clean_text,
+        'tt': 'a',
+        'dm': '5'
+      })
+
+      response = json.loads(urllib2.urlopen(url, params).read())
 
       entities = []
-      for entity in response["entities"]:
-        if entity.has_key("disambiguated"):
-          dbpedia_uri = entity["disambiguated"]["dbpedia"]
-        else:
-          dbpedia_uri = None
-        entities.append((entity["text"], dbpedia_uri))
+      if response['status']['msg'] == 'OK':
+        for e in response['entity_list']:
+          if e.has_key('semld_list'):
+            for uri in e['semld_list']:
+              if 'es.wikipedia' in uri:
+                entities.append((e["form"], uri))
+                break
+              elif 'en.wikipedia' in uri:
+                entities.append((e["form"], uri))
+              else:
+                entities.append((e["form"], None))
 
       feedlist.append(dict(title=result.title, url=result.url, text=clean_text, entities=entities))
     items.append(dict(site=feed[0], feedlist=feedlist))
